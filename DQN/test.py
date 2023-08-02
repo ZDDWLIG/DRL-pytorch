b97210760a519a81778ba0efeb38d17cf7a0f95a
ZDDWLIG <88024901+ZDDWLIG@users.noreply.github.com>
2023-08-02 21:17:57 +0800

    Update test.py

--- DQN/test.py
+++ DQN/test.py
@@ -8,15 +8,13 @@ from random import random,choice,sample
 from collections import deque,namedtuple
 from torch.utils.tensorboard import SummaryWriter
 import os
-from module import DQN,test
+from DDPG_module import DDPG,test
 from matplotlib import pyplot as plt
 path = os.getcwd()
-model_save_path = path + '/results/checkpoints/'
-env_name='LunarLander-v2'
-test_env=gym.make(env_name,render_mode='human')
-
-state_dim=len(test_env.observation_space.sample())
-action_dim=test_env.action_space.n
-agent=DQN(action_dim,state_dim)
-agent.eval_net.load_state_dict(th.load(model_save_path+'model_4500.pth'))
-test(env=test_env,agent=agent,test_episodes=50)
+model_save_path = path + '/results/DDPG/checkpoints/'
+env_name='BipedalWalker-v3'
+env=gym.make(env_name,render_mode='human')
+agent=DDPG(env)
+agent.actor_net.load_state_dict(th.load(model_save_path+'model_4500_actor.pth'))
+agent.critic_net.load_state_dict(th.load(model_save_path+'model_4500_critic.pth'))
+test(env=env,agent=agent,test_episodes=50)
