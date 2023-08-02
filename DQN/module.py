5f3463edcbd8224a5bc7b8c5d0a4be98e6a5a59c
ZDDWLIG <88024901+ZDDWLIG@users.noreply.github.com>
2023-08-02 21:17:21 +0800

    Update module.py

--- DQN/module.py
+++ DQN/module.py
@@ -4,164 +4,149 @@ import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-from random import random,choice,sample
+import random
 from collections import deque,namedtuple
 from torch.utils.tensorboard import SummaryWriter
 import os
 from matplotlib import pyplot as plt
-#Hyperparameter
-lr=1e-3
-episodes=2e6
 device=th.device('cuda' if th.cuda.is_available() else 'cpu')
-gamma=0.99
-batch_size=64
-#Build DQN
-class Q_Net(nn.Module):
-    def __init__(self,action_dim,state_dim):
-        super().__init__()
-        self.fc1=nn.Linear(state_dim,64)
-        self.fc2=nn.Linear(64,128)
-        self.fc3=nn.Linear(128,64)
-        self.fc4=nn.Linear(64,action_dim)
-    def forward(self,x):
-        x=F.relu(self.fc1(x))
-        x=F.relu(self.fc2(x))
-        x=F.relu(self.fc3(x))
-        return self.fc4(x)
-
-
 class ReplayBuffer():
-    def __init__(self,action_dim,mem_size,batch_size):
-        # self.action_dim=action_dim
+    def __init__(self,mem_size,batch_size):
         self.mem_size=mem_size
         self.batch_size=batch_size
         self.memory=deque(maxlen=self.mem_size)
-        self.experience=namedtuple(typename='experience',field_names=['action','state','reward','next_state','done'])
-        #get current length
+        self.experience=namedtuple(typename='experience',field_names=['state','action','reward','next_state','done'])
+    def add(self,state,action,reward,next_state,done):
+        experience=self.experience(state, action, reward, next_state, done)
+        self.memory.append(experience)
     def __len__(self):
         return len(self.memory)
-
-    #add experience to memory
-    def add(self,action,state,reward,next_state,done):
-        experience=self.experience(action,state,reward,next_state,done)
-        self.memory.append(experience)
-
-    #get experiences
     def sample(self):
-        experiences=sample(self.memory,k=self.batch_size)
-        #build batch eg.action->action
-        # actions=np.vstack([e.action for e in experiences if e is not None])
-        # states=np.vstack([e.state for e in experiences if e is not None])
-        # rewards=np.vstack([e.reward for e in experiences if e is not None])
-        # next_states=np.vstack([e.next_state for e in experiences if e is not None])
-        # dones=np.vstack([e.done for e in experiences if e is not None])
-        # actions=th.from_numpy(actions).float().to(device)
-        # states=th.from_numpy(states).float().to(device)
-        # rewards=th.from_numpy(rewards).float().to(device)
-        # next_states=th.from_numpy(next_states).float().to(device)
-        # dones=th.from_numpy(dones.astype(np.uint8)).to(device)
-        states = th.from_numpy(np.vstack([e.state for e in experiences if e is not None])).float().to(device)
-        actions = th.from_numpy(np.vstack([e.action for e in experiences if e is not None])).long().to(device)
-        rewards = th.from_numpy(np.vstack([e.reward for e in experiences if e is not None])).float().to(device)
-        next_states = th.from_numpy(np.vstack([e.next_state for e in experiences if e is not None])).float().to(device)
-        dones = th.from_numpy(np.vstack([e.done for e in experiences if e is not None]).astype(np.uint8)).float().to(device)
+        experiences=random.sample(self.memory,k=self.batch_size)
+        states=th.from_numpy(np.vstack([e.state for e in experiences if e is not None])).float().to(device)
+        actions=th.from_numpy(np.vstack([e.action for e in experiences if e is not None])).float().to(device)
+        rewards=th.from_numpy(np.vstack([e.reward for e in experiences if e is not None])).float().to(device)
+        next_states=th.from_numpy(np.vstack([e.next_state for e in experiences if e is not None])).float().to(device)
+        dones=th.from_numpy(np.vstack([e.done for e in experiences if e is not None])).float().to(device)
 
-        return actions, states, next_states, rewards, dones
+        return states,actions,rewards,next_states,dones
 
 
+class Actor(nn.Module):
+    def __init__(self,  action_dim,state_dim, max_action):
+        super(Actor, self).__init__()
+        self.max_action = max_action
+        self.mlp=nn.Sequential(nn.Linear(state_dim,512),nn.ReLU(),nn.Linear(512,256),nn.ReLU(),nn.Linear(256,action_dim))
+    def forward(self, state):
+        x = self.max_action * th.tanh(self.mlp(state))
+        return x
 
-class DQN():
-    def __init__(self,action_dim,state_dim,batch_size=64,lr=5e-4,gamma=0.99,mem_size=int(1e5),learn_step=5,tau=1e-3,tar_updata=10):
-        #parameters
-        self.action_dim=action_dim
-        self.state_dim=state_dim
+
+class Critic(nn.Module):
+    def __init__(self, state_dim, action_dim):
+        super(Critic, self).__init__()
+
+
+        self.mlp=nn.Sequential(nn.Linear(state_dim + action_dim, 512),nn.ReLU(),nn.Linear(512,256),nn.ReLU(),nn.Linear(256,1))
+    def forward(self, state, action):
+        x = self.mlp(th.cat([state, action], dim=1))
+        return x
+class DDPG():
+    def __init__(self,env,batch_size=128,lr=5e-4,gamma=0.99,noise=0.1,mem_size=int(1e6),tau=5e-3,tar_update=5):
+        self.action_dim = env.action_space.shape[0]
+        self.state_dim = env.observation_space.shape[0]
+        self.min_action = env.action_space.low[0]
+        self.max_action = env.action_space.high[0]
         self.batch_size=batch_size
+        self.lr=lr
         self.gamma=gamma
-        self.learn_step=learn_step
+        self.noise=noise
+        self.mem_size=mem_size
         self.tau=tau
-        self.tar_update=tar_updata
-        #build model
-        self.eval_net=Q_Net(action_dim,state_dim).to(device)
-        self.tar_net=Q_Net(action_dim,state_dim).to(device)
-        self.tar_net.load_state_dict(self.eval_net.state_dict())
-        self.opt=optim.Adam(self.eval_net.parameters(),lr)
+        self.tar_update=tar_update
+        self.actor_net=Actor(self.action_dim,self.state_dim,self.max_action).to(device)
+        self.actor_tar_net=Actor(self.action_dim,self.state_dim,self.max_action).to(device)
+        self.critic_net=Critic(self.action_dim,self.state_dim).to(device)
+        self.critic_tar_net=Critic(self.action_dim,self.state_dim).to(device)
+        self.actor_tar_net.load_state_dict(self.actor_net.state_dict())
+        self.critic_tar_net.load_state_dict(self.critic_net.state_dict())
+        self.replay_buffer=ReplayBuffer(self.mem_size,self.batch_size)
         self.loss=nn.MSELoss()
-        #replay buffer
-        self.memory=ReplayBuffer(action_dim,mem_size,batch_size)
-        self.counter=0
-    #action sample
-    def action_sample(self,state,epsilon):
-        state=th.from_numpy(state).float().unsqueeze(0).to(device)
-        with th.no_grad():
-            action_q_value=self.eval_net(state)
-        #epsilon greedy
-        if random()<epsilon:
-            action=choice(np.arange(self.action_dim))
-        else:
-            action=np.argmax(action_q_value.cpu().data.numpy())
-        return action
-
-    #updata target network [W_tar <-- tau*W_eval+(1-tau)*W_tar]
+        self.actor_net_opt=optim.Adam(self.actor_net.parameters(),1e-4)
+        self.critic_net_opt=optim.Adam(self.critic_net.parameters(),1e-3)
+        self.memory=ReplayBuffer(self.mem_size,self.batch_size)
     def tar_net_update(self):
-        for eval_param, target_param in zip(self.eval_net.parameters(), self.tar_net.parameters()):
+        for eval_param, target_param in zip(self.actor_net.parameters(), self.actor_tar_net.parameters()):
+            target_param.data.copy_(self.tau * eval_param.data + (1.0 - self.tau) * target_param.data)
+        for eval_param, target_param in zip(self.critic_net.parameters(), self.critic_tar_net.parameters()):
             target_param.data.copy_(self.tau * eval_param.data + (1.0 - self.tau) * target_param.data)
 
+    def action_sample(self,state):
+        state=th.from_numpy(state).float().to(device)
+        action=self.actor_net(state).cpu().data.numpy().flatten()
+        action=(action+np.random.normal(0,self.noise,size=self.action_dim)).clip([self.min_action],[self.max_action])
+        return action
+
     def learn(self,experiences,eps):
-        actions, states,next_states , rewards, dones=experiences
-        tar_q=self.tar_net(next_states).detach().max(axis=1)[0].unsqueeze(1)#maxQ* from target network
-        y_t=rewards+self.gamma*tar_q*(1-dones)#TD target
-        eval_q=self.eval_net(states).gather(1,actions.to(int))#Q from eval network
+        states, actions, rewards, next_states, dones=experiences
+        #updata actor network with DPG
+        actor_l=-self.critic_net(states,self.actor_net(states)).mean()
+        self.actor_net_opt.zero_grad()
+        actor_l.backward()
+        self.actor_net_opt.step()
+        #updata critic netwark with TD
+        next_actions=self.actor_tar_net(next_states)
+        tar_q=self.critic_tar_net(next_states,next_actions)
+        y_t=rewards+self.gamma*(1-dones)*tar_q.detach()#target net dont need to backward, we have "tar_net_updata()"
+        eval_q=self.critic_net(states,actions)
+        critic_l=self.loss(eval_q,y_t)
+        self.critic_net_opt.zero_grad()
+        critic_l.backward()
+        self.critic_net_opt.step()
 
-        #backward
-        l=self.loss(eval_q,y_t)
-        self.opt.zero_grad()
-        l.backward()
-        self.opt.step()
-        #update target net
-        if eps %self.tar_update==0:
+        #updata target network
+        if eps%self.tar_update==0:
             self.tar_net_update()
+    def train(self,state,action,reward,next_state,done,eps):
+        self.memory.add(state,action,reward,next_state,done)
+        if len(self.memory)>self.memory.batch_size:
+            experiences=self.memory.sample()
+            self.learn(experiences,eps)
 
 
-    def train(self,action,state,reward,next_state,done,eps):
-        self.memory.add(action,state,reward,next_state,done)
-        self.counter+=1
-        if self.counter%self.learn_step==0:
-            if len(self.memory)>=self.batch_size:
-                experiences=self.memory.sample()
-                self.learn(experiences,eps)
-
-def train(env,agent,episodes=50000,save_inter=500,max_step=1000,epsilon_begin=1.,epsilon_end=0.1,epsilon_decay=0.99,target_score=200,path=os.getcwd()):
+def train(env,agent,episodes=50000,save_inter=500,max_step=200,target_score=250,path=os.getcwd()):
     if th.cuda.is_available():
         print('Lets use gpu!')
     else:
         print('Lets use cpu!')
     score_list=[]
-    eps=epsilon_begin
-    flag = False
-    writer=SummaryWriter(path+'/results/logs')
+    flag=False
+    model_path=path+'/results/DDPG/checkpoints/'
+    log_path=path+'/results/DDPG/logs/'
+    log_writer = SummaryWriter(log_path)
     for ep in range(episodes):
         score=0.
-        state,_=env.reset()
+
+        if not os.path.exists(model_path):
+            os.makedirs(model_path)
+        if not os.path.exists(log_path):
+            os.makedirs(log_path)
+        state,_ =env.reset()
         for step in range(max_step):
-            action=agent.action_sample(state,eps)
-            next_state,reward,done,info,_ =env.step(action)
-            agent.train(action,state,reward,next_state,done,ep)
+            action=agent.action_sample(state)
+            next_state,reward,done,info,_=env.step(action)
+            agent.train(state,action,reward,next_state,done,ep)
             state=next_state
             score+=reward
-            if done:
-                break
         score_list.append(score)
         score_avg=np.mean(score_list[-100:])
-        eps=max(epsilon_end,eps*epsilon_decay)
-        writer.add_scalar('score_avg',score_avg,ep)
-        model_save_path = path + '/results/checkpoints/'
+        log_writer.add_scalar('score_avg',score,ep)
         if ep%save_inter==0:
-            if not os.path.exists(model_save_path):
-                os.makedirs(model_save_path)
-            th.save(agent.eval_net.state_dict(),model_save_path+f'model_{ep}.pth')
-            print('episode:{} average_score={}'.format(ep,score_avg))
-        if len(score_list)>=100:
-            if score_avg>=target_score:
+            th.save(agent.actor_net.state_dict(),model_path+f'model_{ep}_actor.pth')
+            th.save(agent.critic_net.state_dict(),model_path+f'model_{ep}_critic.pth')
+            print('episode={},average_score={}'.format(ep,score_avg))
+        if len(score_list)>-130:
+            if score>target_score:
                 flag=True
                 break
     if flag:
@@ -170,13 +155,14 @@ def train(env,agent,episodes=50000,save_inter=500,max_step=1000,epsilon_begin=1.
         print('Episodes is too small, target is not achieved...')
 
 
+
 def test(env,agent,test_episodes=5,max_step=200):
     score_list=[]
     for ep in range(test_episodes):
         state,_=env.reset()
         score=0.
         for step in range(max_step):
-            action=agent.action_sample(state,epsilon=0)
+            action=agent.action_sample(state)
             next_state,reward,done,info,_=env.step(action)
             state=next_state
             score+=reward
@@ -185,4 +171,10 @@ def test(env,agent,test_episodes=5,max_step=200):
         score_list.append(score)
         score_avg=np.mean(score_list[-100:])
         print(score_avg)
+    # plt.plot([x for x in range(max_step)],score_list)
+    # plt.xlabel('step')
+    # plt.ylabel('average_score')
+    # plt.show()
+
+
 
